94f6427bc1897f23085c46ddf4e3b689e99a85b1
Atsushi Watanabe <atsushi.w@ieee.org>
2019-02-27 14:15:06 +0900

    Disable stack protector to improve performance (#55)

--- build-repo.sh
+++ build-repo.sh
@@ -8,6 +8,15 @@ repodir_base=${REPODIR}
 repo=${1:-backports}
 
 
+# Disable stack protection to improve performance
+
+CFLAGS="-fno-stack-protector -fomit-frame-pointer -march=x86-64 -mtune=generic -Os"
+sudo sed -i "s/export CFLAGS=\".*\"/export CFLAGS=\"${CFLAGS}\"/" /etc/abuild.conf
+
+echo "/etc/abuild.conf:"
+head -n4 /etc/abuild.conf
+
+
 # Overwrite make setting if provided
 
 if [ ! -z "${JOBS}" ]; then
