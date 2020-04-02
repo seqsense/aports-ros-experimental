c83494229d29d5c14e720976911e27c29d2f4ac2
Atsushi Watanabe <atsushi.w@ieee.org>
2020-04-02 12:24:43 +0900

    Tweak py2- packages

--- build-repo.sh
+++ build-repo.sh
@@ -89,6 +89,29 @@ cp -r ${SRCDIR}/* ${aportsdir_base}
 sed -e 's/arch="noarch"/arch="all"/' -i $(find ${aportsdir_base} -name APKBUILD)
 sed -e 's/:noarch//' -i $(find ${aportsdir_base} -name APKBUILD)
 
+
+# Remove python2 builds on new environments
+
+ALPINE_VERSION_MAJOR=$(echo ${ALPINE_VERSION} | cut -f1 -d.)
+ALPINE_VERSION_MINOR=$(echo ${ALPINE_VERSION} | cut -f2 -d.)
+
+echo "ALPINE_VERSION: major(${ALPINE_VERSION_MAJOR}) minor(${ALPINE_VERSION_MINOR})"
+
+ls ${APORTSDIR}
+if [ ${ALPINE_VERSION_MAJOR} -eq 3 -a ${ALPINE_VERSION_MINOR} -ge 11 ]; then
+  echo "Removing py2 from v3.11 aports"
+  py_apkbuilds=$(find $(find ${APORTSDIR}/backports/ -name "py-*" -type d) -name APKBUILD)
+  if [ ! -z "${py_apkbuilds}" ]; then
+    echo "${py_apkbuilds}" | xargs -r -n1 echo "-"
+    sed "s/\<python2-dev\>//g" -i ${py_apkbuilds}              # Remove python2-dev dep
+    sed "s/\<py2-\${pkgname#py-}:_py2\>//g" -i ${py_apkbuilds} # Remove py2- subpackage
+    sed "s/^\s*python2\s/#\0/g" -i ${py_apkbuilds}             # Remove python2 commands
+  else
+    echo "Skipping py2 removal"
+  fi
+fi
+
+
 echo
 echo "Checking version constraint setting"
 find ${aportsdir_base} -name ENABLE_ON | while read path; do
