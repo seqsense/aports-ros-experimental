3b333fa4a540f45e32a2877163f6bb42da01dc4c
Atsushi Watanabe <atsushi.w@ieee.org>
2020-10-10 14:08:51 +0900

    Fix sed syntax

--- test-deps.sh
+++ test-deps.sh
@@ -19,8 +19,7 @@ find /packages -name APKINDEX.tar.gz | while read path; do
   rm -rf ${tmpdir}
 done
 
-sed -i /local_pkgs \
-  '/py2-backports\.ssl_match_hostname/d'
+sed '/^py2-backports\.ssl_match_hostname$/d' -i /local_pkgs
 
 echo
 echo "Installing all local packages"
