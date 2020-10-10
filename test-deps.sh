f5f700d414deef200d74e4ee551d858237de2d4f
Atsushi Watanabe <atsushi.w@ieee.org>
2020-10-10 17:21:47 +0900

    Fix deps test by --force-overwrite

--- test-deps.sh
+++ test-deps.sh
@@ -19,26 +19,8 @@ find /packages -name APKINDEX.tar.gz | while read path; do
   rm -rf ${tmpdir}
 done
 
-# py2-backports.ssl_match_hostname conflicts with py2-backports_functools_lru_cache
-SEPARATED_PKGS="
-  py[23]\{0,1\}-matplotlib
-  py2-backports_functools_lru_cache
-  ros-${ROS_DISTRO}-rqtplot"
-
-touch /separated_pkgs
-for pkg in ${SEPARATED_PKGS}; do
-  sed -n "/^${pkg}\$/p" /local_pkgs >> /separated_pkgs
-  sed "/^${pkg}\$/d" -i /local_pkgs
-done
-
 apk update
 
 echo
-echo "Installing packages"
-apk add --virtual .test $(cat /separated_pkgs)
-echo "Removing packages"
-apk del .test
-
-echo
 echo "Installing all local packages"
-exec apk add $(cat /local_pkgs)
+exec apk add --force-overwrite $(cat /local_pkgs)
