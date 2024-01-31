b09e9d97523cddb5e2c9a223a3b68edee6e867df
Kazuki Takahashi <ktakahashi@seqsense.com>
2024-01-31 11:00:40 +0900

    Dependency of ceres-solver is fixed

--- ros/humble.v3.17/ros-humble-nav2-constrained-smoother/apkbuild_hook.sh
+++
@@ -1,4 +0,0 @@
-apkbuild_hook() {
-  makedepends="${makedepends} glog-dev metis metis-dev suitesparse-dev "
-  depends="${depends} glog-dev metis suitesparse-dev"
-}
--- ros/humble.v3.17/ros-humble-slam-toolbox/apkbuild_hook.sh
+++
@@ -1,4 +0,0 @@
-apkbuild_hook() {
-  makedepends="${makedepends} glog-dev metis metis-dev"
-  depends="${depends} glog-dev metis"
-}
