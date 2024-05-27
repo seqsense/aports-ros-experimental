15eeadee308577f71844efaf2e4b034de336a90e
Atsushi Watanabe <atsushi.w@ieee.org>
2024-05-27 19:35:42 +0900

    Debugging

--- build-repo.sh
+++ build-repo.sh
@@ -112,6 +112,8 @@ echo "REPODIR: ${REPODIR}"
 echo "target repository: ${repo} ${repo_full}"
 echo
 
+ls -l ${SRCDIR}
+
 if [ ! -d ${SRCDIR}/${repo_full} ]; then
   echo "${repo_full} is not present. Skipping."
   exit 0
