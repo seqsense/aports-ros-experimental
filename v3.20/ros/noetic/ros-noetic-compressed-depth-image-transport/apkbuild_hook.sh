549fca89520937905afaf95961f9b92d48fd88c1
Atsushi Watanabe <atsushi.w@ieee.org>
2024-07-23 15:07:41 +0900

    Port as patch

--- v3.20/ros/noetic/ros-noetic-compressed-depth-image-transport/apkbuild_hook.sh
+++ v3.20/ros/noetic/ros-noetic-compressed-depth-image-transport/apkbuild_hook.sh
@@ -1,9 +1,4 @@
 apkbuild_hook() {
   test ${pkgver} = 1.14.0
   pkgver=1.15.0
-  rosinstall="- git:
-      local-name: ${_pkgname}
-      uri: https://github.com/ros-gbp/image_transport_plugins-release.git
-      version: release/noetic/${_pkgname}/1.15.0-1
-  "
 }
